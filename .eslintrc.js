2f19edcb79e794cd560953a97216217442206348
Danil <diamondrock17rocker@gmail.com>
2020-10-19 13:23:53 +0300

    App install & dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  extends: 'react-native-computools',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {'@typescript-eslint/ban-tslint-comment': 0},
+};
---
+++ App.tsx
@@ -0,0 +1,4 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {Text} from 'react-native';
+export const App = () => <Text>123</Text>;
